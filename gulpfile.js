84fdb11edf5b188ea7181550611ff105adf47197
Wanchao Zhang <wonderbeyond@gmail.com>
2015-09-18 22:54:38 +0800

    ajust gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -10,36 +10,43 @@ var swigOpts = {
 }
 
 var ztBaseDir = './zts';
-var ztNames = [];
+var ztNames = fs.readdirSync(ztBaseDir);
 
-fs.readdirSync(ztBaseDir).map(function(ztName) {
-  ztNames.push(ztName);
+var templatesTasks = [];
+var watchTasks = [];
+
+ztNames.forEach(function(ztName) {
   var ztPath = path.join(ztBaseDir, ztName);
   if (fs.statSync(ztPath).isDirectory()) {
-    gulp.task(util.format('%s.templates', ztName), function() {
+    var templatesTask = util.format('%s.templates', ztName);
+    var watchTask = util.format('%s.watch', ztName);
+
+    templatesTasks.push(templatesTask);
+    watchTasks.push(watchTask);
+
+    gulp.task(templatesTask, function() {
       var srcPattern = path.join(ztPath, 'templates', '*.html');
       var destDir = ztPath;
-      console.log(util.format('Src: %s, Dest: %s', srcPattern, ztPath));
 
       gulp.src(srcPattern)
       .pipe(swig(swigOpts))
       .pipe(gulp.dest(ztPath));
-    })
+      console.info(util.format('Compile %s to %s', srcPattern, ztPath));
+    });
+
+    gulp.task(watchTask, function() {
+      var src = [
+        path.join(ztBaseDir, ztName, 'templates', '*.html'),
+        path.join(ztBaseDir, ztName, 'templates', '**/*.html'),
+      ]
+      gulp.watch(src, [util.format('%s.templates', ztName)]);
+      console.info(util.format('Watch on %s', src));
+    });
   }
 })
 
 gulp.task('default', taskListing);
 
-// A task to compile all zt's templates
-gulp.task('templates', ztNames.map(function(n) {return util.format('%s.%s', n, 'templates')}));
-
-gulp.task('watch', function(){
-  // Do separated watch for every zt
-  ztNames.map(function(ztName) {
-    var src = [
-      path.join(ztBaseDir, ztName, 'templates', '*.html'),
-      path.join(ztBaseDir, ztName, 'templates', '**/*.html'),
-    ]
-    gulp.watch(src, [util.format('%s.templates', ztName)]);
-  });
-});
+// All tasks of all zts
+gulp.task('templates', templatesTasks);
+gulp.task('watch', watchTasks);
